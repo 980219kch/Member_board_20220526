7b08d4a5e85e140e332a9b31e958118a70b3dfa5
980219kch <104111005+980219kch@users.noreply.github.com>
2022-05-26 14:29:32 +0900

    회원,게시글,댓글 DTO 작성 완료

---
+++ src/main/java/com/its/memberBoard/dto/MemberDTO.java
@@ -0,0 +1,19 @@
+package com.its.memberBoard.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberDTO {
+    private Long id;
+    private String memberId;
+    private String memberPassword;
+    private String memberName;
+    private String memberEmail;
+    private String memberMobile;
+    private MultipartFile memberProfile;
+}
